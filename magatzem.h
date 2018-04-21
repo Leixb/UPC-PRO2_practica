a28b514a1dedc5114be4baff15826ec258104639
Leix_b <abone9999@gmail.com>
2018-04-21 14:26:44 +0200

    Fixed magatzem.h discrepancy

--- magatzem.h
+++ magatzem.h
@@ -12,19 +12,19 @@ class Magatzem {
 
     std::list<Producte> productes;
 
-    std::map<string, std::list<Producte>::iterator> prod_map;
-    std::vector<unsigned int, Sala*> sala_map;
+    std::map<std::string, std::list<Producte>::iterator> prod_map;
+    std::vector<Sala*> sala_map;
 
     public:
 
     void inicialitza();
 
-    void poner_prod(const string& prod_id);
-    void quitar_prod(const string& prod_id);
+    void poner_prod(const std::string& prod_id);
+    void quitar_prod(const std::string& prod_id);
 
-    unsigned int poner_items(const unsigned int& sala_id, const string& prod_id, const unsigned int& cantidad);
+    unsigned int poner_items(const unsigned int& sala_id, const std::string& prod_id, const unsigned int& cantidad);
 
-    unsigned int distribuir(const string& prod_id, const unsigned int& cantidad);
+    unsigned int distribuir(const std::string& prod_id, const unsigned int& cantidad);
 
     void compactar(const unsigned int& sala_id);
     void reorganizar(const unsigned int& sala_id);
@@ -34,7 +34,7 @@ class Magatzem {
     void escribir(const unsigned int& sala_id) const;
 
     Producte* consultar_pos(const unsigned int& sala_id, const unsigned int& f, const unsigned int& c) const;
-    unsigned int consultar_prod(const string& prod_id) const;
+    unsigned int consultar_prod(const std::string& prod_id) const;
 };
 
 #endif // ifndef MAGATZEM_H
